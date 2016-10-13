5dc420e6aeace813b54e53596b736c6bfa8290a6
rockwolf <thereisanewway@gmail.com>
2016-10-13 22:54:30 +0200

    Final correction and adjusted version number to 0.0.1.

--- ledgerplot.h
+++ ledgerplot.h
@@ -1,7 +1,7 @@
 /* See LICENSE.txt for license and copyright information. */
 #pragma once
 
-#define LEDGERPLOT_VERSION "0.0.0.1"
+#define LEDGERPLOT_VERSION "0.0.1"
 
 enum max_size_t {
     MS_INPUT_LINE = 1024, /* MAX line length to copy */
