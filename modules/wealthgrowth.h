281a66e82ccbb395975ca41141c7b8b6795fc6ca
rockwolf <thereisanewway@gmail.com>
2017-06-22 19:39:23 +0200

    First set of changes to make the wealthgrowth report work.

--- modules/wealthgrowth.h
+++ modules/wealthgrowth.h
@@ -3,7 +3,8 @@
 
 int wealthgrowth_prepare_temp_file(
     const char *a_input_file,
-    FILE *a_output_file,
+    FILE *a_output_file0,
+    FILE *a_output_file1,
     uint32_t a_start_year,
     uint32_t a_end_year,
     enum enum_plot_timeframe_t a_plot_timeframe_t
